d866c0c1e57d163ad53b6cb68f89180835b8df73
arafato <oliver.arafat@outlook.com>
2014-10-13 19:28:26 +0200

    Added knockout nuget packages, added protected observable (custom).

---
+++ js/ko-extensions/customObservables.js
@@ -0,0 +1,32 @@
+﻿ko.protectedObservable = function (initialValue) {
+    //private variables
+    var _actualValue = ko.observable(initialValue),
+        _tempValue = initialValue;
+
+    //computed observable that we will return
+    var result = ko.computed({
+        //always return the actual value
+        read: function () {
+            return _actualValue();
+        },
+        //stored in a temporary spot until commit
+        write: function (newValue) {
+            _tempValue = newValue;
+        }
+    }).extend({ notify: "always" });
+
+    //if different, commit temp value
+    result.commit = function () {
+        if (_tempValue !== _actualValue()) {
+            _actualValue(_tempValue);
+        }
+    };
+
+    //force subscribers to take original
+    result.reset = function () {
+        _actualValue.valueHasMutated();
+        _tempValue = _actualValue();   //reset temp value
+    };
+
+    return result;
+};
