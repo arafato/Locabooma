74c94c4599e7df63a01d879c4e713f5f89b9ba1b
arafato <oliver.arafat@outlook.com>
2014-10-13 19:41:06 +0200

    added old version from LocationBookmarker app, commented out phonegap specific stuff.

---
+++ js/vm.js
@@ -0,0 +1,131 @@
+﻿WinJS.Namespace.define("Models", {
+    bookmark: function (title, description) {
+        var self = this;
+        // Also serves as ID
+        this.date = Date.now();
+        this.title = ko.observable(title);
+        this.description = ko.observable(description);
+        this.longitude = ko.observable(0.0);
+        this.latitude = ko.observable(0.0);
+        this.image = ko.observable(""); // encoded string
+        this.getFormattedDate = function () {
+            return new Date(self.date);
+        }
+    },
+
+    bmManager: function (entries) {
+        var self = this;
+        // self.db = storage.storageUnit.instance();
+        self.bookmarks = ko.observableArray(entries);
+        self.currentBookmark = ko.observable(new bookmark());
+        self.deleteBookmark = function () {
+            var index = self.bookmarks.indexOf(self.currentBookmark());
+            self.bookmarks.splice(index, 1);
+            self.db.remove(self.currentBookmark().date);
+            //window.location = "#overview";
+        }
+        self.createBookmark = function () {
+            var bmark = new bookmark("New Bookmark", "");
+            //navigator.geolocation.getCurrentPosition(function (position) {
+            //    bmark.longitude(position.coords.longitude);
+            //    bmark.latitude(position.coords.latitude);
+            //},
+            //function (error) {
+            //    alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+            //});
+            self.currentBookmark(bmark);
+            return true;
+        }
+        self.saveBookmark = function () {
+            self.bookmarks.push(self.currentBookmark());
+            $("#overview").trigger("create");
+            var date = self.currentBookmark().date;
+            var obj = ko.toJSON(self.currentBookmark());
+            self.db.set(date, obj, function () { }, function (msg) { alert("Error saving bookmark in storage: " + msg) });
+            //window.location = "#overview";
+        }
+        self.selectBookmark = function () {
+            self.currentBookmark(this);
+            return true;
+        }
+        self.takePhoto = function () {
+            //navigator.camera.getPicture(function (imageData) {
+            //    var encodedImage = "data:image/jpeg;base64," + imageData;
+            //    var image = document.getElementById("image");
+            //    image.src = encodedImage;
+            //    self.currentBookmark().image(encodedImage);
+            //},
+            //function (message) {
+            //    alert('Failed because: ' + message);
+            //},
+            //{
+            //    quality: 50,
+            //    destinationType: Camera.DestinationType.DATA_URL
+            //});
+        }
+        self.launchMap = function () {
+            var longitude = self.currentBookmark().longitude();
+            var latitude = self.currentBookmark().latitude();
+            //window.external.notify("LocationBookmarker.Maps:" + longitude + "," + latitude);
+            //navigator.plugins.nativemap.show(function (msg) { }, function (msg) { alert(msg); }, [longitude, latitude]);
+        }
+    }
+});
+
+function vm(entries) {
+    var self = this;
+    self.db = storage.storageUnit.instance();
+    self.bookmarks = ko.observableArray(entries);
+    self.currentBookmark = ko.observable(new bookmark());
+    self.deleteBookmark = function () {
+        var index = self.bookmarks.indexOf(self.currentBookmark());
+        self.bookmarks.splice(index, 1);
+        self.db.remove(self.currentBookmark().date);
+        window.location = "#overview";
+    }
+    self.createBookmark = function () {
+        var bmark = new bookmark("New Bookmark", "");
+        navigator.geolocation.getCurrentPosition(function (position) {
+            bmark.longitude(position.coords.longitude);
+            bmark.latitude(position.coords.latitude);
+        },
+        function (error) {
+            alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+        });
+        self.currentBookmark(bmark);
+        return true;
+    }
+    self.saveBookmark = function () {
+        self.bookmarks.push(self.currentBookmark());
+        $("#overview").trigger("create");
+        var date = self.currentBookmark().date;
+        var obj = ko.toJSON(self.currentBookmark());
+        self.db.set(date, obj, function () { }, function (msg) { alert("Error saving bookmark in storage: " + msg) });
+        window.location = "#overview";
+    }
+    self.selectBookmark = function () {
+        self.currentBookmark(this);
+        return true;
+    }
+    self.takePhoto = function () {
+        navigator.camera.getPicture(function (imageData) {
+            var encodedImage = "data:image/jpeg;base64," + imageData;
+            var image = document.getElementById("image");
+            image.src = encodedImage;
+            self.currentBookmark().image(encodedImage);
+        },
+        function (message) {
+            alert('Failed because: ' + message);
+        },
+        {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL
+        });
+    }
+    self.launchMap = function () {
+        var longitude = self.currentBookmark().longitude();
+        var latitude = self.currentBookmark().latitude();
+        //window.external.notify("LocationBookmarker.Maps:" + longitude + "," + latitude);
+        navigator.plugins.nativemap.show(function (msg) { }, function (msg) { alert(msg); }, [longitude, latitude]);
+    }
+}
