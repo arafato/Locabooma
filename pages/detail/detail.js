518d9b0188686057540792de747e4b829d6aff8c
arafato <oliver.arafat@outlook.com>
2014-10-21 22:32:11 +0200

    added detail pagecontrol (skeleton only)

---
+++ pages/detail/detail.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    WinJS.UI.Pages.define("/pages/detail/detail.html", {
+        ready: function (element, options) {
+            console.log(options);
+            // TODO: bind current bookmark (in options) to view
+            // appBar.showOnlyCommands(['detail...']);
+        },
+
+        unload: function () {
+            console.log("unloading");
+        }
+    });
+})();
