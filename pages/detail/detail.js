b6167c5417b3a7978ce511a2d9adb420733e61fe
arafato <oliver.arafat@outlook.com>
2014-11-01 21:01:46 +0100

    added template design for structuring code in the WinJS way (resembles a MVC design)

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -4,13 +4,23 @@
             console.log(options);
             // TODO: bind current bookmark (in options) to view
             // appBar.showOnlyCommands(['detail...']);
+            
+            //button.addEventListener("click", function (e) {
+            //    options.print();
+            //})
 
-            var button = document.getElementById("showlocmark");
-            button.addEventListener("click", function (e) {
-                options.print();
-            })
+            this.setupEventHandlers(options);
 
             WinJS.Binding.processAll(document.getElementById("detailpage"), options);
+        },
+
+        setupEventHandlers: function(vm) {
+            var button = document.getElementById("showlocmark");
+            button.addEventListener("click", this.someAction(vm));
+        },
+
+        someAction: function(vm) {
+            vm.print();
         },
 
         unload: function () {
