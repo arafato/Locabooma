316fce995ee3f31608a213569383b361a4e4a3e8
arafato <oliver.arafat@outlook.com>
2014-11-17 22:23:21 +0100

    added two-way binding mechanism for WinJS.

---
+++ js/winjs/binding.js
@@ -0,0 +1,12 @@
+﻿// Two-Way Binding
+// Call as <input type="text" data-win-bind="value: name Binding.Mode.twoway" />
+WinJS.Namespace.define("Binding.Mode", {
+    twoway: WinJS.Binding.initializer(function (source, sourceProps, dest, destProps) {
+        WinJS.Binding.defaultBind(source, sourceProps, dest, destProps);
+        dest.onchange = function () {
+            var d = dest[destProps[0]];
+            var s = source[sourceProps[0]];
+            if (s !== d) source[sourceProps[0]] = d;
+        }
+    })
+});
